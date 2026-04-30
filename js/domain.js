b59b9ba28fe9b2af56ab6389701813fb7f893d08
root <root@localhost.localdomain>
2026-04-30 12:44:01 +0800

    chore: update API domain to worldcat-richard-walks-fairfield.trycloudflare.com

--- js/domain.js
+++ js/domain.js
@@ -1,5 +1,5 @@
 // 自动生成的域名配置
-// 生成时间: 2026-04-30T12:39:01+08:00
+// 生成时间: 2026-04-30T12:44:01+08:00
 // 更新域名: worldcat-richard-walks-fairfield.trycloudflare.com
 
 var API_DOMAIN = "worldcat-richard-walks-fairfield.trycloudflare.com";
